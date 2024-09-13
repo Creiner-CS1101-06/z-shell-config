e4f4e21b4655ef08de41d0de7c9f781b88f56ccc
Alex Creiner <142434892+alexbcreiner0@users.noreply.github.com>
2024-09-13 14:11:45 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 git clone https://github.com/Creiner-CS1101-06/z-shell-config.git ~/z-shell-config
-[ -f ~/.config/zsh/.zshrc ] && rm ~/.config/zsh/.zshrc
-cp ~/z-shell-config/zshrc\(windows\) ~/.config/zsh/.zshrc
+[ -f ~/.zshrc ] && rm ~/.zshrc
+cp ~/z-shell-config/zshrc\(windows\) ~/.zshrc
 [ -f ~/.config/starship.toml ] && rm ~/.config/starship.toml
 cp ~/z-shell-config/starship.toml ~/.config/starship.toml
-source ~/.config/zsh/.zshrc
+source ~/.zshrc
 rm -rf ~/z-shell-config
