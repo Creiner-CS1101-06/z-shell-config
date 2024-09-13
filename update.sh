7b2b203acadd2513d084687351c779ba3dec2171
Alex Creiner <alexbcreiner@gmail.com>
2024-09-13 14:50:52 -0400

    Added a special secret option for myself

--- update.sh
+++ update.sh
@@ -15,6 +15,11 @@ case "$answer" in
         echo "You have selected Windows"
         cp ~/z-shell-config/zshrc\(windows\) ~/.zshrc
         ;;
+    "alex" )
+        echo "You chose alex"
+        cp ~/z-shell-config/zshrc.professor ~/.zshrc
+        exit 1
+        ;;
     *)
         echo "Invalid answer. Please run the script again and  either type the letter m if you are a Mac-OS user or w if you are a Windows/WSL/Linux user."
         exit 1
