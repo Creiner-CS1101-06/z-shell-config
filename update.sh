04dd3ad219b3dce492b9c74c522daf24a9f67662
Alex Creiner <alexbcreiner@gmail.com>
2024-09-13 14:37:14 -0400

    Added a prompt to select windows or mac-os

--- update.sh
+++ update.sh
@@ -1,8 +1,26 @@
 #!/bin/zsh
 
+echo "Are you Mac-OS (m) or Windows/WSL/Linux (w)?"
+read answer
+
 git clone https://github.com/Creiner-CS1101-06/z-shell-config.git ~/z-shell-config
 [ -f ~/.zshrc ] && rm ~/.zshrc
-cp ~/z-shell-config/zshrc\(windows\) ~/.zshrc
+
+case "$answer" in
+    [Mm]* )
+        echo "You have selected Mac-OS."
+        cp ~/z-shell-config/zshrc\(mac-os\) ~/.zshrc
+        ;;
+    [Ww]* )
+        echo "You have selected Windows"
+        cp ~/z-shell-config/zshrc\(windows\) ~/.zshrc
+        ;;
+    *)
+        echo "Invalid answer. Please run the script again and  either type the letter m if you are a Mac-OS user or w if you are a Windows/WSL/Linux user."
+        exit 1
+        ;;
+esac
+
 [ -f ~/.config/starship.toml ] && rm ~/.config/starship.toml
 cp ~/z-shell-config/starship.toml ~/.config/starship.toml
 source ~/.zshrc
