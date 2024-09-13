56c91956b7e9853752f03f23144535889cabefe8
Alex Creiner <alexbcreiner@gmail.com>
2024-09-13 16:22:36 -0400

    Why is this so hard

--- update.sh
+++ update.sh
@@ -25,8 +25,7 @@ case "$answer" in
         # for func in $(typeset -f | awk '/^[a-zA-Z_][a-zA-Z0-9_]* \(\)/ {print $1}'); do
         #     unset -f $func  # Remove all functions
         # done
-        cp ~/z-shell-config/zshrc.professor ~/.config/zsh/.zshrc
-        source ~/.config/zsh/.zshrc
+        cp ~/z-shell-config/zshrc.professor ~/.config/zsh/.zshrc && source ~/.config/zsh/.zshrc
         ;;
     *)
         echo "Invalid answer. Please run the script again and  either type the letter m if you are a Mac-OS user or w if you are a Windows/WSL/Linux user."
